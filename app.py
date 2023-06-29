8136948e5f610404fc750df043035a0a0dfba935
Kartikey Pandey <pandeykh@mail.uc.edu>
2023-06-28 23:41:26 -0400

    Added upload files functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, send_file, request
 import os
 
 app=Flask(__name__,template_folder='template')
@@ -12,56 +12,94 @@ def portfolio():
     return render_template('portfolio.html')
 
 @app.route('/generate', methods=['POST'])
-def generate(): 
+def generate():
+    if request.method == 'POST':  
+        resume = request.files['resume']
+        resume.save(os.path.join(app.root_path, 'static/' + resume.filename ))
+
+        headshot = request.files['headshot']
+        headshot.save(os.path.join(app.root_path, 'static/' + headshot.filename ))
+    
     name = request.form['name']
     number = request.form['number']
     email = request.form['email']
+    summary = request.form['summary']
     degree = request.form['degree']
     major = request.form['major']
     university = request.form['university']
     school_start = request.form['school-start']
     school_end = request.form['school-end']
+    school_misc = request.form['school-misc']
+    skill1 = request.form['skill1']
+    skill1_prof = request.form['skill1-prof']
+    skill2 = request.form['skill2']
+    skill2_prof = request.form['skill2-prof']
+    skill3 = request.form['skill3']
+    skill3_prof = request.form['skill3-prof']
+    skill4 = request.form['skill4']
+    skill4_prof = request.form['skill4-prof']
+    skill5 = request.form['skill5']
+    skill5_prof = request.form['skill5-prof']
+    skill6 = request.form['skill6']
+    skill6_prof = request.form['skill6-prof']
     title1 = request.form['title1']
-    location1 = request.form['location1']
+    name_location1 = request.form['name-location1']
     work_start1 = request.form['work-start1']
     work_end1 = request.form['work-end1']
     work_description1 = request.form['work-description1']
     title2 = request.form['title2']
-    location2 = request.form['location2']
+    name_location2 = request.form['name-location2']
     work_start2 = request.form['work-start2']
     work_end2 = request.form['work-end2']
     work_description2 = request.form['work-description2']
     title3 = request.form['title3']
-    location3 = request.form['location3']
+    name_location3 = request.form['name-location3']
     work_start3 = request.form['work-start3']
     work_end3 = request.form['work-end3']
     work_description3 = request.form['work-description3']
     
     # Generate the HTML content for the portfolio
-    rendered_content = render_template('portfolio-template.html', 
+    rendered_content = render_template('portfolio-template.html',
                                    name=name,
                                    number=number,
-                                   email=email,                                   
+                                   email=email,
+                                   headshot=headshot,
+                                   resume=resume,
+                                   summary=summary,
                                    degree=degree,
                                    major=major,
                                    university=university,
                                    school_start=school_start,
                                    school_end=school_end,
+                                   school_misc=school_misc,
+                                   skill1=skill1,
+                                   skill1_prof=skill1_prof,
+                                   skill2=skill2,
+                                   skill2_prof=skill2_prof,
+                                   skill3=skill3,
+                                   skill3_prof=skill3_prof,
+                                   skill4=skill4,
+                                   skill4_prof=skill4_prof,
+                                   skill5=skill5,
+                                   skill5_prof=skill5_prof,
+                                   skill6=skill6,
+                                   skill6_prof=skill6_prof,
                                    title1=title1,
-                                   location1=location1,
+                                   name_location1=name_location1,
                                    work_start1=work_start1,
                                    work_end1=work_end1,
                                    work_description1=work_description1,
                                    title2=title2,
-                                   location2=location2,
+                                   name_location2=name_location2,
                                    work_start2=work_start2,
                                    work_end2=work_end2,
                                    work_description2=work_description2,
                                    title3=title3,
-                                   location3=location3,
+                                   name_location3=name_location3,
                                    work_start3=work_start3,
                                    work_end3=work_end3,
                                    work_description3=work_description3)
+
 
     
     
